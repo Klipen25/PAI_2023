18d23b4b7149e9fb9fc5e4c4a30bb2d7680d68ca
Jędrzej Gąsiorowski (Uczeń) <jgasiorowski143@technikumkreatywne.pl>
2022-09-13 11:50:34 +0200

    13.09.2022

---
+++ Stronka.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+</head>
+<body>
+
+	<h1>This is a Heading</h1>
+	<p>This is a paragraph.</p>
+	
+	<?php
+	$txt = "1";
+	echo "Mam całe $txt pomarańczy<br>";
+	echo "Lubie tą $txt pomarańcz<br>";
+	echo "Nie mam $txt pomarańczy <br>";
+	?>
+
+</body>
+</html>
