731cae27d4730d1409eab9f8730d4b5cb0cdd23f
Jędrzej Gąsiorowski (Uczeń) <jgasiorowski143@technikumkreatywne.pl>
2022-09-21 14:50:14 +0200

    21.09.2022

---
+++ Lekcja 2 łączenie PHP i MSQL/JGasiorwoski/zadaniaPAI.php
@@ -0,0 +1,90 @@
+<html>
+<head>
+    <title> PAI </title>
+</head>
+<body>
+
+
+
+    <?php
+	/*
+    $ocena = 76;
+	if ($ocena >= 0 && $ocena <=39){
+		echo "1";
+	}
+	elseif ($ocena >= 40 && $ocena <=54){
+		echo "2";
+	}
+	elseif ($ocena >= 55 && $ocena <=69){
+		echo "3";
+	}
+	elseif ($ocena >= 70 && $ocena <=84){
+		echo "4";
+	}
+	elseif ($ocena >= 85 && $ocena <=94){
+		echo "5";
+	}
+	elseif ($ocena >= 95 && $ocena <=100){
+		echo "6";
+	}
+	
+	$wynik = 76;
+	if ($wynik >= 0 && $wynik <=39){
+		$wynik = 1;
+	}
+	elseif ($wynik >= 40 && $wynik <=54){
+		$wynik = 2;
+	}
+	elseif ($wynik >= 55 && $wynik <=69){
+		$wynik = 3;
+	}
+	elseif ($wynik >= 70 && $wynik <=84){
+		$wynik = 4;
+	}
+	elseif ($wynik >= 85 && $wynik <=94){
+		$wynik = 5;
+	}
+	elseif ($wynik >= 95 && $wynik <=100){
+		$wynik = 6;
+	}
+	
+	
+	switch($wynik):
+	
+	case 1:
+	echo "Ocena to: ", $wynik;
+	break;
+	
+	case 2:
+	echo "Ocena to: ", $wynik;
+	break;
+	
+	case 3:
+	echo "Ocena to: ", $wynik;
+	break;
+	
+	case 4:
+	echo "Ocena to: ", $wynik;
+	break;
+	
+	case 5:
+	echo "Ocena to: ", $wynik;
+	break;
+	
+	case 6:
+	echo "Ocena to: ", $wynik;
+	break;
+	
+	endswitch;
+    ?>
+	*/
+	
+	for($liczba = -11; $liczba <= 11; ++$liczba)
+	{
+		if($liczba % 5 == 0) continue;
+		echo $liczba, "<br>";
+	}
+
+?>
+</body>
+</html>
